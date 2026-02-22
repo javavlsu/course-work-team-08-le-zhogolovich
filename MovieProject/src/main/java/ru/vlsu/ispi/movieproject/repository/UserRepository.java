2d32d0d36433740c74dca7b4f670235aeb91fbf0
griplingg <griplingg@yandex.ru>
2026-02-22 16:57:55 +0300

    Добавлен JavaDoc для UserRepository

--- MovieProject/src/main/java/ru/vlsu/ispi/movieproject/repository/UserRepository.java
+++ MovieProject/src/main/java/ru/vlsu/ispi/movieproject/repository/UserRepository.java
@@ -5,7 +5,31 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+/**
+ * Репозиторий для работы с сущностью {@link User}.
+ *
+ * <p>
+ * Предоставляет стандартные CRUD-операции для пользователей,
+ * а также методы поиска по уникальным полям.
+ * Реализация интерфейса автоматически создаётся Spring Data JPA
+ * </p>
+ */
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    /**
+     * Находит пользователя по адресу электронной почты
+     *
+     * @param email адрес электронной почты
+     * @return Optional с найденным пользователем
+     *         или Optional.empty(), если пользователь не найден
+     */
     Optional<User> findbyEmail(String email);
+
+    /**
+     * Проверяет существование пользователя с указанным email.
+     *
+     * @param email адрес электронной почты
+     * @return true — если пользователь существует, иначе false
+     */
     boolean existsByEmail(String email);
 }
