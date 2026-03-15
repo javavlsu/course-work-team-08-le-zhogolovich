e64cdbff4af95b50cdc17bc0676a02e98f5a10b9
griplingg <griplingg@yandex.ru>
2026-03-16 00:07:24 +0300

    добавлен черновик главной страницы, добавлен конфиг для связи с реакт приложением

--- MovieProject/backend/src/main/java/ru/vlsu/ispi/movieproject/controller/MovieController.java
+++ MovieProject/backend/src/main/java/ru/vlsu/ispi/movieproject/controller/MovieController.java
@@ -1,6 +1,7 @@
 package ru.vlsu.ispi.movieproject.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/movies")
+@CrossOrigin(origins = "http://localhost:3000")
 public class MovieController {
     private final MovieService movieService;
 
